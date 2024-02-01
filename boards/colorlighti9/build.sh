3b3e6d6c843d71017911dca01dffe91d58d17850
Ângelo Renato Pazin Malaguti <82290484+llTurtle22ll@users.noreply.github.com>
2024-02-01 17:39:43 -0300

    Update build.sh
    added missing file

--- boards/colorlighti9/build.sh
+++ boards/colorlighti9/build.sh
@@ -1,4 +1,4 @@
 rm -f darksocv.cfg  darksocv.json
-yosys -D __YOSYS__ -D LATTICE_ECP5_COLORLIGHTI9 -p "synth_ecp5 -json darksocv.json -top darksocv" ../../rtl/darksocv.v ../../rtl/darkriscv.v ../../rtl/darkuart.v pll_ref_25MHz.v
+yosys -D __YOSYS__ -D LATTICE_ECP5_COLORLIGHTI9 -p "synth_ecp5 -json darksocv.json -top darksocv" ../../rtl/darksocv.v ../..rtl/darkpll.v ../../rtl/darkriscv.v ../../rtl/darkuart.v pll_ref_25MHz.v
 nextpnr-ecp5 --timing-allow-fail --json darksocv.json --textcfg darksocv.cfg --45k --package CABGA381 --speed 6 --lpf darksocv.lpf
 ecppack --compress --input darksocv.cfg --bit darksocv.bit
